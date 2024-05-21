be757142e71f5f983c4c445f4f61cc2ed9ea3c87
edgarbfilho <edgarbfilho@gmail.com>
2024-05-21 23:35:42 +0000

    Adicionando api.php web.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BootstrapReactController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CadastroController;
 
@@ -10,11 +8,8 @@ Route::get('/teste', function () {
     return response()->json(['message' => 'This is a test route']);
 });
 
-Route::apiResource('bootstraps', BootstrapReactController::class);
-
-// Em routes/api.php
-Route::post('/login', [AuthController::class, 'login']); // Usando sintaxe de classe diretamente
-Route::post('/register', [AuthController::class, 'register']); // Usando sintaxe de classe diretamente
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
 
@@ -25,13 +20,3 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('cadastros', CadastroController::class);
     
 });
-
-
-// Route::post('/oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
-// Route::post('/oauth/token/revoke', '\Laravel\Passport\Http\Controllers\TransientTokenController@revoke');
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-
